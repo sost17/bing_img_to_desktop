08f77ceb76d562769c5f1ec4951beca71a69f2b1
colloc <common17@outlook.com>
2022-04-06 18:14:34 +0800

    Update use_Bingimage_for_desktop.py

--- use_Bingimage_for_desktop.py
+++ use_Bingimage_for_desktop.py
@@ -1,40 +1,46 @@
-#coding = utf-8
+# coding = utf-8
 
 import os
 import win32api
 import win32con
 import win32gui
 import requests
-from bs4 import BeautifulSoup
+from lxml import etree
 
-baseurl ='http://www.bing.com'
+baseurl = 'http://www.bing.com'
 imagename = r'C:/Users/Public/desktopimage.jpg'
 file = ''
 
+
 def get_bingimg(url):
     req = requests.get(url)
-    soup = BeautifulSoup(req.text,'html.parser')
-    Link = soup.find_all(id="bgLink")
-    for href in Link:
+    selector = etree.HTML(req.text)
+    Link = selector.xpath('//link[@id="preloadBg"]/@href')
+    if Link:
+        img_url = Link[0]
         print('找到Bing壁纸')
-        downloadimagefile(baseurl + href.get('href'))
-    
+        downloadimagefile(img_url)
+
 
 def downloadimagefile(url):
-    img_req=requests.request(method='GET',url=url,stream='true').content
-    file = open(imagename,'wb')
-    file.write(img_req)
-    file.close()
+    img_req = requests.request(method='GET', url=url, stream='true').content
+
+    if os.path.exists(imagename):
+        os.remove(imagename)
+        print('图片删除成功')
+
+    with open(imagename, 'wb') as wfile:
+        wfile.write(img_req)
     print('图片下载成功')
 
+
 def changedesktopimage():
-    key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,"Control Panel\\Desktop",0,win32con.KEY_SET_VALUE)
-    win32api.RegSetValueEx(key,"WallpaperStyle",0,win32con.REG_SZ,"2")
-    win32api.RegSetValueEx(key,"TileWallpaper",0,win32con.REG_SZ,"0")
-    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER,imagename,1+2)
+    key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
+    win32api.RegSetValueEx(key, "WallpaperStyle", 0, win32con.REG_SZ, "2")
+    win32api.RegSetValueEx(key, "TileWallpaper", 0, win32con.REG_SZ, "0")
+    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, imagename, 1 + 2)
     print('桌面壁纸设置成功')
-    os.remove(imagename)
-    print('图片删除成功')
+
 
 if __name__ == '__main__':
     get_bingimg(baseurl)
